dcab6364df26cd0deda6c8443f5ecd14e326d01d
Viraj <viraj@Vishnus-iMac.local>
2018-01-31 19:35:15 +0530

    first commit

---
+++ src/com/viraj/Converter/Converter.java
@@ -0,0 +1,22 @@
+package com.viraj.Converter;
+
+public class Converter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float farenheit = 65f;
+		System.out.println(farenheit);
+		
+		farenheit = (((farenheit - 32) / 9) * 5);
+		System.out.println(farenheit);
+		
+		int a, b, c, z = 5;
+		a = b = c = z;
+		
+		System.out.println(a + " " + b + " " + c + " " + z);
+		
+
+	}
+
+}
